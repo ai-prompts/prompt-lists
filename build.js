daa5f77a255e73ab83c8ca52e7fb4ad9ce2a98e5
Paul <fofr@users.noreply.github.com>
2025-02-16 15:12:51 +0000

    Standard formatting

--- build.js
+++ build.js
@@ -1,33 +1,33 @@
-import listHelpers from "./index.js";
-import fs from "fs";
+import listHelpers from './index.js'
+import fs from 'fs'
 
-const allLists = [];
-const listMetadata = {};
+const allLists = []
+const listMetadata = {}
 
 // Simple deterministic random number generator with fixed seed
 function seededRandom(seed) {
-  return function () {
+  return function() {
     seed = (seed * 9301 + 49297) % 233280;
     return seed / 233280;
-  };
+  }
 }
 
 const random = seededRandom(100);
 
 for (const category of Object.keys(listHelpers)) {
   for (const listName of Object.keys(listHelpers[category])) {
-    const key = `${category}.${listName}`;
-    const data = listHelpers[category][listName]();
+    const key = `${category}.${listName}`
+    const data = listHelpers[category][listName]()
 
-    allLists.push(key);
+    allLists.push(key)
     listMetadata[key] = {
       ...data,
       total: data.list.length,
-      examples: data.list.sort(() => random() - 0.5).slice(0, 2),
-    };
-    delete listMetadata[key].list;
+      examples: data.list.sort(() => random() - 0.5).slice(0, 2)
+    }
+    delete listMetadata[key].list
   }
 }
 
-fs.writeFileSync("lists.json", JSON.stringify(allLists, null, 2));
-fs.writeFileSync("list-metadata.json", JSON.stringify(listMetadata, null, 2));
+fs.writeFileSync('lists.json', JSON.stringify(allLists, null, 2))
+fs.writeFileSync('list-metadata.json', JSON.stringify(listMetadata, null, 2))
